4bd3a124a2d407ad99a23e712fce9ae2bd1ff80c
ZJ <leezijun1@gmail.com>
2025-05-06 16:04:27 +0800

    Updated BMI code

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+def calcBMI(height, weight):
+    bmi = weight/height**2
+    print("height is ", height)
+    print("weight is ", weight)
+    print("bmi is ", bmi)
+    return bmi
+
+def classifyBMI(bmi):
+    if(bmi>=30.0):
+        return 1
+    elif(bmi<30.0 and bmi>25.0):
+        return 0
+    elif(bmi>=18.5 and bmi<=25.0):
+        return -1
+    
+    return
+
+def app():
+    output = calcBMI(1.78,86)
+    classifyBMI(output)
+    return
+
+if __name__ == "__main__":
+    app()
